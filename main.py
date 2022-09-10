0b3fb00ad74f3bdf75aef4c96f4f598e9812796f
ty-martz <jacksontmartz@gmail.com>
2022-09-10 13:07:38 -0400

    update main with routes and functions

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import mysql.connector
 
 from scripts.emission_calc import emission_calculator
-from scripts.airport_dist_calc import calculate_distance
+from scripts.airport_dist_calc import calculate_distance, load_airport_data
 
 app = Flask(__name__)
 
@@ -37,19 +37,62 @@ mycursor = mydb.cursor()
 # Root URL
 @app.route('/')
 def index():
-     # Set The upload HTML template '\templates\index.html'
-    return render_template('index.html')
+    # Set The upload HTML template '\templates\index.html'
+    mycursor.execute("select count(*) from flights")
+    flights_in_db = mycursor.fetchone()[0]
+    #print('----')
+    #print(mycursor[0])
+    #print('----')
+    if flights_in_db is None:
+      flights_in_db = 0
+    #try:
+    mycursor.execute("select sum(emissions) from flight_data.flights")
+    total_emissions = float(mycursor.fetchone()[0])
+    #except:
+    #  total_emissions = 0
+    return render_template('index.html', n_flights = flights_in_db, total_em=total_emissions)
+
+# Single Entry URL
+@app.route('/single', methods=['GET', 'POST'])
+def single():
+      # load airport codes
+      airp = load_airport_data()
+      iata_codes = airp['iata_code']
+      # Calculate single entry with html inputs
+      og_air = request.form.get('origin-select')
+      de_air = request.form.get('destination-select')
+      if (og_air is not None) & (de_air is not None):
+            n_pax = int(request.form.get('num-pax'))
+            rt = request.form.get('round-trip')
+            r_force = request.form.get('rad-force')
+            print(rt)
+            print(r_force)
+
+            dist = calculate_distance(og_air, de_air)
+            emiss = round(emission_calculator(dist, n_pax, exclude_rad_force=r_force, round_trip=rt), 2)
+            emiss = f'{emiss} tonnes of carbon from your flight.'
+      else:
+            emiss = ''
+      # Set The upload HTML template '\templates\index.html'
+      return render_template('single.html', emiss_out=emiss, air_codes=iata_codes)
+
+# Bulk Entry URL
+@app.route('/bulk')
+def bulk():
+      # Set The upload HTML template '\templates\index.html'
+      return render_template('bulk.html')
 
 # Results URL
 @app.route('/results')
 def results():
      # Set The upload HTML template '\templates\index.html'
-    return render_template('results.html')
+    total_dist = request.args.get('total_dist')
+    total_em = request.args.get('total_em')
+    return render_template('results.html', total_d=total_dist, total_e=total_em)
 
 # template URL
 @app.route("/getTemplate")
 def getTemplate():
-      print(os.getcwd())
       with open("templates/artifacts/flights_template.csv") as fp:
             csv = fp.read()
       return Response(
@@ -60,7 +103,7 @@ def getTemplate():
 
 
 # Get the uploaded files
-@app.route("/", methods=['POST'])
+@app.route("/bulk", methods=['POST'])
 def uploadFiles():
       # get the uploaded file
       uploaded_file = request.files['flights_file']
@@ -68,9 +111,12 @@ def uploadFiles():
            file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
           # set the file path
            uploaded_file.save(file_path)
-           parseCSV(file_path)
+           dataframe, td, te = parseCSV(file_path)
           # save the file
-      return redirect(url_for('results'))
+      else:
+            td = 0.001
+            te = 0.001
+      return redirect(url_for('results', total_dist=td, total_em=te))
 
 def parseCSV(filePath):
       # Use Pandas to parse the CSV file
@@ -88,7 +134,7 @@ def parseCSV(filePath):
              value = (row['origin_airport'],row['destination_airport'],row['class'],row['round_trip'],row['usd_amount'], dist, emiss)
              mycursor.execute(sql, value)
              mydb.commit()
-             print(i,row['origin_airport'],row['destination_airport'],row['class'],row['round_trip'],row['usd_amount'], dist, emiss)
+             #print(i,row['origin_airport'],row['destination_airport'],row['class'],row['round_trip'],row['usd_amount'], dist, emiss)
       
       df['distance_miles'] = miles_list
       df['emissions'] = emiss_list
